b343a146b9d269876e1e077fc5392f26cc2730c0
Mariano Soffredi <nanospe2k2@gmail.com>
2023-02-10 21:35:59 -0300

    Página para listar TODOS
    Se simuló la petición de los Todos
    Se agregaron botones para borrar que activan eventos.
    Se agregó un botón para agregar Todos que activa un evento
    No están implementados los modals ni el formato de la página

--- frontend/main.js
+++ frontend/main.js
@@ -1,12 +1,50 @@
 async function getTodos(url){
-    const todos = await fetch(url);
-    if(todos.status !== 200){
-        return `Status: ${todos.status}`
-    }
-    return todos.json()
-}
+    const toDos = await fetch(url);
+    return toDos.json()
+};
 
 getTodos("todos.json")
 .then((response) => {
-    console.log(response)
+    for(let todo of response){
+        displayToDo(todo);
+    }
+});
+
+function displayToDo({title, id}){
+    const a = document.createElement("a");
+    a.classList.add("todo");
+    a.dataset.id = id;
+
+    const content = document.createElement("div");
+    content.classList.add("todo-content");
+    
+    const foot = document.createElement("div");
+    foot.classList.add("todo-foot");
+    
+    const header = document.createElement("h1");
+    header.textContent = title;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "DELETE";
+    deleteButton.classList.add("delete");
+
+    //Delete button "onClick" event
+    deleteButton.addEventListener('click', (e) => {
+        document.body.removeChild(a)
+        console.log(`Open modal to delete element with id of ${id}`)
+    })
+    
+    //appends
+    content.append(header);
+    foot.append(deleteButton);
+    
+    a.append(content);
+    a.append(foot);
+
+    document.body.append(a);
+};
+
+const newToDo = document.querySelector('.new-todo');
+newToDo.addEventListener('click', (e) => {
+    displayToDo({title: 'Nuevo TODO!', id: "0123"})
 })
