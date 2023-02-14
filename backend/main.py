8b809166016659e04c4d92f4a17b259b5e4b11c6
Luis Albornoz <luisalbornoz2001@gmail.com>
2023-02-14 16:45:11 -0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,48 @@
-print("Hello world!")
-print("Hello world!")
+from fastapi import  Response, FastAPI, HTTPException, status
+from typing import List, Optional
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class tasks(BaseModel):
+    id: int
+    title : str
+    completed : bool
+
+class item_task(BaseModel):
+    id : int
+    created_at : datetime
+    title : str
+    color : str
+    task : List[tasks]
+
+
+app = FastAPI()
+
+
+@app.post("/todo", response_model=item_task)
+def created_todo(item:  item_task):
+    return item
+
+
+@app.get("/todos", response_model=item_task)
+def get_todos():
+    return item_task (
+    id = 1,
+    created_at = 2009/7/12,
+    title = "Compras",
+    color = "Red",
+    task = [{
+        "id": 2,
+        "title" : "compras",
+        "completed" : True,
+    },
+    {
+        "id": 3,
+        "title" : "ventas",
+        "completed" : False,
+    }
+    ]
+)
+
+
