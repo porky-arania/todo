73ec42658c044482e7bd407c1607e8f07884f393
Luis Albornoz <107951868+LuisAlbornoz@users.noreply.github.com>
2023-02-20 23:46:33 -0300

    GET Todos List Method (#19)
    * add_author
    
    Agregando nuevo autor al proyecto
    
    * Update main.py
    
    * get_method
    
    create method to creat new todo
    
    * New corrections.
    
    New models module.
    Post method deleted.
    Format section fixed.
    Doc Strings added.
    
    Co-Authored-By: Ian-Calderon <81045828+Ian-Calderon@users.noreply.github.com>
    Co-Authored-By: Luis Albornoz <107951868+LuisAlbornoz@users.noreply.github.com>
    Co-Authored-By: JoseBron <110634432+JoseBron@users.noreply.github.com>
    
    * Corrections added.
    
    Fixed import List, because it's not required (Python 3.11.1).
    Fixed created_at in main.py module.
    
    Co-Authored-By: Luis Albornoz <107951868+LuisAlbornoz@users.noreply.github.com>
    Co-Authored-By: JoseBron <110634432+JoseBron@users.noreply.github.com>
    Co-Authored-By: Ian-Calderon <81045828+Ian-Calderon@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Valentin Minolli <minolli.vale@gmail.com>
    Co-authored-by: Ian-Calderon <81045828+Ian-Calderon@users.noreply.github.com>
    Co-authored-by: JoseBron <110634432+JoseBron@users.noreply.github.com>

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,30 @@
-print("Hello world!")
-print("Hello world!")
+from fastapi import FastAPI
+
+from models import Todo
+
+from datetime import datetime
+
+app = FastAPI()
+
+
+@app.get("/todos", response_model=Todo)
+def get_todos():
+    """Get a list of todos."""
+    return Todo(
+        id=1,
+        created_at=datetime.now(),
+        title="Compras",
+        color="Red",
+        task=[
+            {
+                "id": 2,
+                "title": "compras",
+                "completed": True,
+            },
+            {
+                "id": 3,
+                "title": "ventas",
+                "completed": False,
+            },
+        ],
+    )
