814f9d0812290b9ddb4d21aa4a29e50dc35f18be
Valentin Minolli <minolli.vale@gmail.com>
2023-02-28 21:06:06 -0300

    Final Version of Post Method
    Restructure the code in main.py and models.py files.
    Added missing parametters.
    Imported uuid library.
    Added database.py, services.py and selectors.py files for future usage.
    
    Co-Authored-By: Luis Albornoz <107951868+LuisAlbornoz@users.noreply.github.com>
    Co-Authored-By: JoseBron <110634432+JoseBron@users.noreply.github.com>
    Co-Authored-By: Ian-Calderon <81045828+Ian-Calderon@users.noreply.github.com>

--- backend/main.py
+++ backend/main.py
@@ -1,36 +1,19 @@
 from fastapi import FastAPI
 
-from models import Todo, Task
-
-from datetime import datetime
+from models import Todo
 
 
 app = FastAPI()
 
 
-def create_todo(
-        *,
-        title: str = "A Todo Title",
-        color: str = "Red",
-        task: list[Task] | None = None,
-):
-    """Create and return a sample todo."""
-    if tasks is None:
-            tasks = []
-
-    todo = Todo(...)
-    return todo 
-
-
 @app.get("/todos", response_model=Todo)
 def get_todos():
     """Return a list of todos."""
-    todo = create_todo()
+    todo = Todo.create_todo()
     return todo
 
 
 @app.post("/todo", response_model=Todo)
-def post_todo(todo:Todo):
+def post_todo(todo: Todo):
     """Create and return a custom todo."""
     return todo
-
--- backend/models.py
+++ backend/models.py
@@ -2,18 +2,41 @@ from pydantic import BaseModel
 
 from datetime import datetime
 
+import uuid
+
 
 class Task(BaseModel):
     """Task object."""
-    id: int
+
+    id: str
     title: str
     completed: bool
 
 
 class Todo(BaseModel):
     """Todo object."""
-    id: int
+
+    id: str
     created_at: datetime
     title: str
     color: str
     task: list[Task]
+
+    def create_todo(
+        *,
+        title: str = "A Todo Title",
+        color: str = "Red",
+        task: list[Task] | None = None,
+    ):
+        """Create and return a sample todo."""
+        if tasks is None:
+            tasks = []
+
+        todo = Todo(
+            id=uuid.uuid4(),
+            created_at=datetime.now(),
+            title=title,
+            color=color,
+            task=task,
+        )
+        return todo
