344f1786aa441e67de175c5fe455238a07aef526
Valentin Minolli <minolli.vale@gmail.com>
2023-03-07 16:40:12 -0300

    POST Method & Database.
    I added the feature to save the created todo into the database.

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,20 @@
 print("Hello, world!")
+from fastapi import FastAPI
 
 print("Valentín Minolli estuvo aquí")
+from models import Todo
+
+
+app = FastAPI()
+
+
+@app.post("/todo", response_model=Todo)
+def post_todo_endpoint(todo: Todo):
+    """Create and return a Todo with the given data."""
+    todo = Todo.create_todo(
+          title=todo.title,
+        color=todo.color,
+        task=todo.task
+    )
+    return todo
+
