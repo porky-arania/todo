e5d62f61fe11de20cc8b64eda439d9f8dc76251b
Valentin Minolli <122620062+ValentinMinolli@users.noreply.github.com>
2023-03-07 18:52:29 -0300

    Database Connection Added. (#24)
    * Adding my Tests - VM
    
    * Test de Valentin
    
    * Database Connection Added.
    
    I added the Database connection so we can store objects in a database.
    
    * POST Method & Database.
    
    I added the feature to save the created todo into the database.
    
    * Post Method & Database Connection.
    
    I added the feature to save the created todo into the database.
    
    * GET List of Todos Method.
    
    Method adapted to obtain a list of Todos that are stored in the database.
    
    * GET Specific Todo Method.
    
    https://trello.com/c/9aDVyEYO/22-crear-un-endpoint-para-listar-un-todo-espec%C3%ADfico
    Method adapted to obtain a specific Todo that is stored in the database.
    
    * PUT Method Added.
    
    https://trello.com/c/UKryHi0C/10-crear-un-endpoint-para-editar-un-todo
    New method added to modify a specific Todo that is stored in the database.
    
    * DELETE Method Added.
    
    https://trello.com/c/EjojPGnO/9-crear-un-endpoint-para-eliminar-un-todo
    103 / 5,000
    Resultados de traducción
    Traducción
    Added new method to delete a specific Todo that is stored in the database.

--- backend/main.py
+++ backend/main.py
@@ -6,14 +6,40 @@ from models import Todo
 app = FastAPI()
 
 
-@app.get("/todos", response_model=Todo)
-def get_todos():
-    """Return a list of todos."""
-    todo = Todo.create_todo()
-    return todo
-
-
 @app.post("/todo", response_model=Todo)
-def post_todo(todo: Todo):
-    """Create and return a custom todo."""
+def post_todo_endpoint(todo: Todo):
+    """Create and return a Todo with the given data."""
+    todo = Todo.create_todo(
+          title=todo.title,
+        color=todo.color,
+        task=todo.task
+    )
     return todo
+
+
+@app.get("/todos")
+def get_list_of_todos_endpoint():
+    """Return a list of Todos in the system."""
+    todos = Todo.get_list_of_todos()
+    return todos
+
+
+@app.get("/todo/{id}")
+def get_single_todo_endpoint(id:str):
+    """Return a single todo with the given ID."""
+    todo = Todo.get_single_todo(id)
+    return todo
+
+
+@app.put("/todo/{id}", response_model=Todo)
+def update_todo_endpoint(id: str, todo_data:Todo):
+    """Update and return an updated Todo with the given ID using the new data."""
+    new_data = todo_data.dict(exclude_unset=True)
+    updated_todo = Todo.update_todo(id, new_data)
+    return updated_todo
+
+
+@app.delete("/todo/{id}")
+def delete_single_todo_endpoint(id:str):
+    """Delete a single Todo with the given ID."""
+    Todo.delete_todo(id)
--- backend/models.py
+++ backend/models.py
@@ -4,39 +4,75 @@ from datetime import datetime
 
 import uuid
 
+from database import client
+
+from pymongo import ReturnDocument
+
 
 class Task(BaseModel):
     """Task object."""
 
-    id: str
+    id: str = str(uuid.uuid4())
     title: str
-    completed: bool
+    completed: bool = False
 
 
 class Todo(BaseModel):
     """Todo object."""
 
-    id: str
-    created_at: datetime
+    id: str = str(uuid.uuid4())
+    created_at: datetime = datetime.now()
     title: str
     color: str
     task: list[Task]
 
     def create_todo(
         *,
-        title: str = "A Todo Title",
-        color: str = "Red",
+        title: str = "A Todo Title.",
+        color: str = "Red.",
         task: list[Task] | None = None,
     ):
-        """Create and return a sample todo."""
-        if tasks is None:
-            tasks = []
+        """Create and return a todo with the given data."""
+        if task is None:
+            task = []
 
-        todo = Todo(
-            id=uuid.uuid4(),
-            created_at=datetime.now(),
-            title=title,
-            color=color,
-            task=task,
+        todo_data = Todo(
+            id = str(uuid.uuid4()),
+            created_at = datetime.now(),
+            title = title,
+            color = color,
+            task = task,
         )
+
+        todo = todo_data.dict()
+        client.local.todo.insert_one(todo)
         return todo
+        
+    def get_list_of_todos():
+        """Get and return a list of Todos in the system."""
+        todos = list(client.local.todo.find())
+        for todo in todos:
+            if todo:
+                todo['_id'] = str(todo['_id'])
+        return todos
+
+    def get_single_todo(id:str):
+        """Get and return a specific Todo with the given ID."""
+        todo = client.local.todo.find_one({'id': id})
+
+        if todo:
+            todo['_id'] = str(todo['_id'])
+        return todo
+
+    def update_todo(id:str, new_data: dict):
+        """Update and return a Todo with the given ID using the new data."""
+        result = client.local.todo.find_one_and_update(
+            {'id': id},
+            {'$set': new_data},
+            return_document=ReturnDocument.AFTER,
+        )
+        return result
+
+    def delete_todo(id:str):
+        """Delete a single Todo with the given ID."""
+        client.local.todo.delete_one({'id': id})
