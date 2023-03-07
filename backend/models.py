b5f2b5181736f39cde02150e990a306e51e47d50
Valentin Minolli <minolli.vale@gmail.com>
2023-03-07 16:56:00 -0300

    Post Method & Database Connection.
    I added the feature to save the created todo into the database.

---
+++ backend/models.py
@@ -0,0 +1,47 @@
+from pydantic import BaseModel
+
+from datetime import datetime
+
+import uuid
+
+from database import client
+
+
+class Task(BaseModel):
+    """Task object."""
+
+    id: str = str(uuid.uuid4())
+    title: str
+    completed: bool = False
+
+
+class Todo(BaseModel):
+    """Todo object."""
+
+    id: str = str(uuid.uuid4())
+    created_at: datetime = datetime.now()
+    title: str
+    color: str
+    task: list[Task]
+
+    def create_todo(
+        *,
+        title: str = "A Todo Title.",
+        color: str = "Red.",
+        task: list[Task] | None = None,
+    ):
+        """Create and return a todo with the given data."""
+        if task is None:
+            task = []
+
+        todo_data = Todo(
+            id = str(uuid.uuid4()),
+            created_at = datetime.now(),
+            title = title,
+            color = color,
+            task = task,
+        )
+
+        todo = todo_data.dict()
+        client.local.todo.insert_one(todo)
+        return todo
